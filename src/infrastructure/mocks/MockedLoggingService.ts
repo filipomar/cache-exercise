606e983a4fd637a59c01ee9f9e732f28c6ad519d
Filipe Pomar <filipe.pomar@happycar.de>
2019-10-06 14:55:28 +0200

    ïmplemented logging service

--- src/infrastructure/mocks/MockedLoggingService.ts
+++ src/infrastructure/mocks/MockedLoggingService.ts
@@ -1,9 +1,7 @@
 import { LoggingService } from "../../application/services/LoggingService";
 
 export class MockedLoggingService implements LoggingService {
-	info(information: string): Promise<void> {
-		throw new Error("Method not implemented.");
+	public async info(information: string): Promise<void> {
+		console.log(information);
 	}
-
-
 }
