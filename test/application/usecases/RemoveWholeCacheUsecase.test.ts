6852cf0257c84b72a7625c324afef37b55760f44
Filipe Pomar <filipe.pomar@happycar.de>
2019-10-06 15:57:37 +0200

    added tests for remove whole cache usecase

---
+++ test/application/usecases/RemoveWholeCacheUsecase.test.ts
@@ -0,0 +1,26 @@
+import { MockedCacheService } from "../../../src/infrastructure/mocks/MockedCacheService";
+
+import { RemoveWholeCacheUsecase } from "../../../src/application/usecases/RemoveWholeCacheUsecase";
+
+test('Validate that the previously existing key is being removed', async () => {
+	const cacheService = new MockedCacheService();
+	await cacheService.setCache('KEY', 'VALUE');
+
+	expect(await cacheService.getCache('KEY')).not.toBeNull();
+
+	const usecase = new RemoveWholeCacheUsecase(cacheService);
+	await usecase.invoke();
+
+	expect(await cacheService.getCache('KEY')).toBeNull();
+});
+
+test('Validate that the previously non existing key is not causing an exception', async () => {
+	const cacheService = new MockedCacheService();
+
+	expect(await cacheService.getCache('KEY')).toBeNull();
+
+	const usecase = new RemoveWholeCacheUsecase(cacheService);
+	await usecase.invoke();
+
+	expect(await cacheService.getCache('KEY')).toBeNull();
+});
