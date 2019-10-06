a4149e77a3e2e1510d0e0603dab51c222705a209
Filipe Pomar <filipe.pomar@happycar.de>
2019-10-06 14:54:47 +0200

    ïmplemented mocked slow data service

--- src/infrastructure/mocks/MockedDataService.ts
+++ src/infrastructure/mocks/MockedDataService.ts
@@ -1,7 +1,11 @@
+import { generate } from "randomstring";
 import { DataService } from "../../application/services/DataService";
 
+const delay = (ms: number) => new Promise<void>((resolve) => setTimeout(resolve, ms));
+
 export class MockedDataService implements DataService {
-	fetch(cacheKey: string): Promise<string> {
-		throw new Error("Method not implemented.");
+	public async fetch(cacheKey: string): Promise<string> {
+		await delay(Math.random() * 1000);
+		return generate({ readable: true, length: 100 });
 	}
 }
