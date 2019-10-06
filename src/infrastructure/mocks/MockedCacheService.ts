da5a553a84f0c55e8a2c00a6c76e62156ac51b44
Filipe Pomar <filipe.pomar@happycar.de>
2019-10-06 14:50:20 +0200

    implemented mocked cache servers

--- src/infrastructure/mocks/MockedCacheService.ts
+++ src/infrastructure/mocks/MockedCacheService.ts
@@ -1,12 +1,15 @@
 import { CacheService } from "../../application/services/CacheService";
 
 export class MockedCacheService implements CacheService {
-	setCache(cacheKey: string, data: string): Promise<void> {
-		throw new Error("Method not implemented.");
+
+	private cache = new Map<string, string>();
+
+	public async setCache(cacheKey: string, data: string): Promise<void> {
+		this.cache.set(cacheKey, data);
 	}
 
-	getCache(cacheKey: string): Promise<string | null> {
-		throw new Error("Method not implemented.");
+	public async getCache(cacheKey: string): Promise<string | null> {
+		return this.cache.get(cacheKey) || null;
 	}
 
 
